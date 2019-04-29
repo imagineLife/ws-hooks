8f7ed673184aa7ad24bbcb6509920db56bbeb072
Eric Laursen <ericjlaursen@gmail.com>
2019-04-29 09:50:10 -0400

    sets socket start && stop listeners

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import Title from './components/Title'
+import io from 'socket.io-client'
+// import Title from './components/Title'
 
 const App = ({pText}) => {
-  let titleText = 'app title text!'
+	let [socket, setSocket] = useState(null)
+	let [connected, setConnected] = useState(false)
+
+	//takes fn && arr
+	useEffect(() => {
+		//connect socket server
+		setSocket(io('http://localhost:3000'))
+		setConnected(true)
+		
+		//like componnetWillUnMount
+		return () => {
+			socket.on('disconnect', setConnected(false))
+		}
+	}, [])
+
+	// useEffect(()=> {
+	// 	// //add a listener to the socket for each event
+	// 	socket.on('connect', setConnected(true));
+	// })
+
+	// useEffect(() => {
+	// 	socket.on('disconnect', setConnected(false))
+	// })
+
   	return (
-	    <Title text={titleText} />
+	    <p>Connected: {connected == true ? 'true' : 'false'}</p>
 	  );
 };
 
