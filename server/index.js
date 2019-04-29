e2000376165a2ee5cc9fa76cef8eee8927e40446
Eric Laursen <ericjlaursen@gmail.com>
2019-04-29 07:14:45 -0400

    begins socket server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+//middlewares
+//serve static files via public folder
+app.use(express.static('./dist'));
+
+//start app
+var httpServer = app.listen(3000)
+
+//create a socket server
+var io = require('socket.io').listen(httpServer)
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
+console.log('server running!')
