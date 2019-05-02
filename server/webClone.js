ec1a1bfde2cf3432953dc652598314beb6028ec0
Eric Laursen <ericjlaursen@gmail.com>
2019-05-02 12:31:07 -0400

    more logs, updated event names

--- server/webClone.js
+++ server/webClone.js
@@ -4,31 +4,39 @@ const port = 3011;
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-io.on('connection', socket => {
+let users = [];
+
+io.on('connection', connectedSocket => {
   console.log('a user connected');
   
-  socket.on('disconnect', reason => {
+  console.log('connectedSocket.id')
+  console.log(connectedSocket.id)
+  
+  connectedSocket.on('disconnect', reason => {
     console.log('user disconnected');
   });
 
-  socket.on('room', data => {
-    console.log('room join');
+  connectedSocket.on('join-room', data => {
+    console.log('joining');
     console.log(data);
-    socket.join(data.room);
+    connectedSocket.join(data.room);
   });
 
-  socket.on('leave room', data => {
+  connectedSocket.on('leave-room', data => {
     console.log('leaving room');
     console.log(data);
-    socket.leave(data.room)
+    connectedSocket.leave(data.room)
   });
 
-  socket.on('new message', data => {
+  connectedSocket.on('new-message', data => {
     console.log(data.room);
-    socket.broadcast
+    connectedSocket.broadcast
     .to(data.room)
     .emit('receive message', data)
   });
+
+  console.log('// - - - - - //')
+  
 });
 
 server.listen(port);
