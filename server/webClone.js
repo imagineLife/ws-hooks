24ebed303fc9dddb38c5f8e2573a00bd16854e71
Eric Laursen <ericjlaursen@gmail.com>
2019-05-04 20:06:22 -0400

    more logs

--- server/webClone.js
+++ server/webClone.js
@@ -4,7 +4,8 @@ const port = 3011;
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-let users = [];
+let usersInRoom = [];
+let rooms = {}
 
 io.on('connection', connectedSocket => {
   console.log('a user connected');
@@ -19,17 +20,27 @@ io.on('connection', connectedSocket => {
   connectedSocket.on('join-room', data => {
     console.log('joining');
     console.log(data);
+    io.sockets.emit('share-users-in-room', usersInRoom)
+    rooms[data.room] = [];
+    rooms[data.room].push(connectedSocket.id)
     connectedSocket.join(data.room);
   });
 
   connectedSocket.on('leave-room', data => {
     console.log('leaving room');
     console.log(data);
+    usersInRoom.filter(usr => usr !== connectedSocket.id)
+    io.sockets.emit('share-users-in-room', usersInRoom)
     connectedSocket.leave(data.room)
   });
 
   connectedSocket.on('new-message', data => {
-    console.log(data.room);
+    console.log('new-message recieved')
+    console.log(data);
+    console.log('connectedSocket.rooms')
+    console.log(connectedSocket.rooms)
+    
+    
     connectedSocket.broadcast
     .to(data.room)
     .emit('receive message', data)
